216a7a936466b8d819e8b1f012057bd99d4721c2
Prithvi Singh <prithvisingh77772209@gmail.com>
2026-08-08 18:37:24 +0530

    Hybrid inheritence
    Diamon Problem using virtual keyword

---
+++ Unit-2/Diamond_Problem.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    int x = 10;
+};
+
+class B : virtual public A
+{
+};
+
+class C : virtual public A
+{
+};
+
+class D : public B, public C
+{
+};
+
+int main()
+{
+    D obj;
+
+    cout << obj.x << endl;  // No ambiguity
+
+    return 0;
+}
